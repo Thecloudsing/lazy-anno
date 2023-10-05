b5d1150a56171a38d6bb961ed5b9158012a65b66
unknown <1756174331@qq.com>
2023-10-05 23:04:25 +0800

    Processor-1.0

--- src/main/resources/source/SpElParser.java
+++ src/main/resources/source/SpElParser.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 public class SpElParser {
     private static final SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
+
     public static String out(String spel, Map<String, Object> map) {
         Expression expression = spelExpressionParser.parseExpression(spel, new TemplateParserContext("${", "}"));
         EvaluationContext evaluationContext = new StandardEvaluationContext();
